3cdcd6cc9a8f8fbf2a1ab0efd2daf7658930ca92
GMercury <kingorigithinji@gmail.com>
2022-03-15 17:55:24 +0300

    put char c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * Return:0
+ * main - Prints _putchar as a message.
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-char c[10] = "main";
-int i = 0;
-while (i < 9)
-{
-_putchar(c[i]);
-i++;
-}
-_putchar('\n');
-return (0);
+	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
+	int count, sz;
+
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
+	return (0);
 }
