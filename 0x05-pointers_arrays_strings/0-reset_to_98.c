d9a28fd53dec54b8523175e7635eef557f5c7afb
GMercury <kingorigithinji@gmail.com>
2022-03-21 21:23:58 +0300

    takes a pointer to an int as parameter and updates the value it points to to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,16 +1,13 @@
 #include "main.h"
 
 /**
- * swap_int -  a function that swaps the values of two integers.
- * @a: an inout integer pointer
- * @b: an input integer pointer
- * Return: Nothing
+ * reset_to_98 - a function that takes a pointer to an int as parameter
+ * and updates the value it points to to 98.
+ * @n: An integer input pointer
+ * Return: Nothing.
  */
-void swap_int(int *a, int *b)
-{
-	int aux;
 
-	aux = *a;
-	*a = *b;
-	*b = aux;
+void reset_to_98(int *n)
+{
+	*n = 98;
 }
