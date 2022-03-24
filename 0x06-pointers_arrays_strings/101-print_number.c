38ad78d556de47a1b8ff6e301dbd8de4bdf3cbf5
GMercury <kingorigithinji@gmail.com>
2022-03-24 03:22:19 +0300

    its alive!!!! and prints an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,22 +1,26 @@
 #include "main.h"
+
 /**
- * print_number - prints an integer
- * @n:.input integer parameter
- *
- *
- */
+  * print_number - print numbers chars
+  * @n: integer params
+  * Return: 0
+ **/
+
 void print_number(int n)
 {
-	unsigned int i = n;
+	unsigned int n1;
+
+	n1 = n;
 
 	if (n < 0)
 	{
-		_putchar(45);
-		i = -i;
+		_putchar('-');
+		n1 = -n;
 	}
-	if (i / 10)
+
+	if (n1 / 10 != 0)
 	{
-		print_number(i / 10);
+		print_number(n1 / 10);
 	}
-	_putchar(i % 10 + '0');
+	_putchar((n1 % 10) + '0');
 }
