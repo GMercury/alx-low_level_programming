f8b03da4418a7d0dafa6464bd03a7184d33d36de
GMercury <kingorigithinji@gmail.com>
2022-03-24 21:00:26 +0300

    its alive!!!! and prints an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,9 +1,9 @@
 #include "main.h"
-  
+
 /**
- * print_number - print numbers chars
- * @n: integer params
- * Return: 0
+  * print_number - print numbers chars
+  * @n: integer params
+  * Return: 0
  **/
 
 void print_number(int n)
