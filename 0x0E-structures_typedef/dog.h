1e5d4962d57d9c18d012adf5478bdb83510e266f
GMercury <kingorigithinji@gmail.com>
2022-04-11 19:56:49 +0300

    add task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,1 +1,17 @@
-##
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - represents a dog with name, age and owner
+ * @name: name of dog as string
+ * @age: age of the dog as float
+ * @owner: owner of the dog as string
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
