a9c82cfa5dd4dba1d380689d1729e3e2f6bf4764
GMercury <kingorigithinji@gmail.com>
2022-04-11 21:16:52 +0300

    adding task 5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,1 +1,23 @@
-#
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dogs
+ * @d: dog_t to be freed
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	/* free name field */
+	if (d->name != NULL)
+		free(d->name);
+
+	/* free owner field */
+	if (d->owner != NULL)
+		free(d->owner);
+
+	/* free dog */
+	free(d);
+}
