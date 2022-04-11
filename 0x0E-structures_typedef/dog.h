2ae87c3224bf719fce6316327a4a79edbbfd78d4
GMercury <kingorigithinji@gmail.com>
2022-04-11 21:22:21 +0300

    adding task 2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,24 +1,15 @@
-#ifndef DOG_H
-#define DOG_H
+#include "dog.h"
 
 /**
- * struct dog - represents a dog with name, age and owner
+ * init_dog - initializes a variable of type struct dog
+ * @d: address of a struct dog
  * @name: name of dog as string
- * @age: age of the dog as float
- * @owner: owner of the dog as string
+ * @age: age of dog as float
+ * @owner: owner of dog as string
  */
-struct dog
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	char *name;
-	float age;
-	char *owner;
-};
-
-typedef struct dog dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
-#endif
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
