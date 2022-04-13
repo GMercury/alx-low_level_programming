b6b8b2c567f81138eef9a23c0a8e3642f63fde75
GMercury <kingorigithinji@gmail.com>
2022-04-13 21:30:21 +0300

    update finction pointers h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,9 @@
-#include <stddef.h>
-
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
