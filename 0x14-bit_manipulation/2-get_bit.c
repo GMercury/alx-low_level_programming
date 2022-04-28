3d30a8b0cd14828e3614a7fb6462a67a1d9d70f2
GMercury <kingorigithinji@gmail.com>
2022-04-28 19:04:59 +0300

    0x14-bit_manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,7 +9,8 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int mask = (1 << index);
+	if (index >= (sizeof(n)) * 8)
+		return (-1);
 
-	return ((n & mask) > 0);
+	return ((n & (1 << index)) > 0);
 }
