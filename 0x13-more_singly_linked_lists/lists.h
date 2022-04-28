dc166fb9f09fdb825b7560de12b8a3610ca0c4e1
GMercury <kingorigithinji@gmail.com>
2022-04-28 18:05:44 +0300

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,6 +18,20 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/**
+ * struct listnode_s - singly linked list
+ * @ptr: address of listint_t
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listnode_s
+{
+	listint_t *ptr;
+	struct listnode_s *next;
+} listnode_t;
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -29,5 +43,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *reverse_listint(listint_t **head);
+listnode_t *add_nodeptr(listnode_t **head, const listint_t *ptr);
+void free_listnode(listnode_t *head);
+int is_in_nodes(listnode_t *head, const listint_t *ptr);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
 
 #endif
